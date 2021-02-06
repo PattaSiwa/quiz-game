6c95fe50c7b392748aaebe05bd3cfe114c97a960
Thay <pattasiwa@gmail.com>
2021-02-06 11:13:09 -0500

    created help me modal, renamed some files

--- js/modal.js
+++ js/modal.js
@@ -1,7 +1,30 @@
 //Start Modal
-const startBtn = document.querySelector('#quizStart')
+const startBtn = document.querySelector('#startQuiz')
 
 startBtn.addEventListener('click', () => {
     document.querySelector('#startModal').style.display = 'none'
 
 })
+
+
+//Help Modal 
+
+const helpBtn = document.querySelector('#helpMe')
+
+helpBtn.addEventListener('click', () => {
+    document.querySelector('#helpModal').style.display = 'block'
+})
+
+const closeBtn = document.querySelector('.closeBtn')
+
+closeBtn.addEventListener('click', () => {
+    document.querySelector('#helpModal').style.display = 'none'
+})
+
+const restartBtn = document.querySelector('.restartQuiz')
+// there will be another restart button --- note to make sure don't mix them up
+
+restartBtn.addEventListener('click', () => {
+    //doesnt do anythign yet
+})
+

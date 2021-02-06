96e8e26d67be322ae184bb21613d878ab20473dd
Thay <pattasiwa@gmail.com>
2021-02-06 10:54:24 -0500

    created openning Modal

---
+++ js/modal.js
@@ -0,0 +1,7 @@
+//Start Modal
+const startBtn = document.querySelector('#quizStart')
+
+startBtn.addEventListener('click', () => {
+    document.querySelector('#startModal').style.display = 'none'
+
+})

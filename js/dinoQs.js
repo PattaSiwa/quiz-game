3c273dd538705db0214eb010864b5d11090099d5
Thay <pattasiwa@gmail.com>
2021-02-10 14:34:21 -0500

    fixed minor CSS issues, added dino questions-- dino subject completed

--- js/dinoQs.js
+++ js/dinoQs.js
@@ -6,9 +6,18 @@ const dino5 = new Question("What is the name of this Dinosaur?", 'dino/5.jpg', '
 const dino6 = new Question("What is the name of this Dinosaur?", 'dino/6.jpg', 'Spinosaurus')
 const dino7 = new Question("What is the name of this Dinosaur?", 'dino/7.jpg', 'Ankylosaurus')
 const dino8 = new Question("What is the name of this Dinosaur?", 'dino/8.jpg', 'Allosaurus')
+const dino9 = new QuestionType2("What was the heaviest Dinosaur?", 'dino/9.jpg', 'Argentinosaurus', 'Branchiosaurus', 'Stegosaurus', 'Allosaurus')
+const dino10 = new QuestionType2("When did Dinosaurs go extinct?", 'dino/11.jpeg', '65 million years', '5,000 years', '35 million years', '200 million years')
+const dino11 = new QuestionType2("How long ago did the Dinosaurs started roaming the earth?", 'dino/10.jpeg', '245 million years', '35 million years', '400 million years', '80,000 years')
+const dino12 = new QuestionType2("Dinosaurs belong to which group of animals?", 'dino/12.png', 'Reptiles', 'Amphibians', 'Mammals', 'Insects')
+const dino13 = new QuestionType2("About how many species of Dinosuars have been named?", 'dino/13.jpg', '700', '50', '300', '1,000')
+const dino14 = new QuestionType2("What likely caused the extinction of Dinosaurs?", 'dino/14.jpg', 'Meteor and Volcanic Eruptions', 'Alien Invasion', 'Supernova', 'Cataracts')
+const dino15 = new QuestionType2("A scientist that studies Dinosaur fossils is called?", 'dino/16.jpeg', 'Paleontologist', 'Chemist', 'Physicist', 'Astronomer')
+const dino16 = new TrueOrFalse("Human lived along side Dinosaurs?", 'dino/15.jpg', 'False')
+const dino17 = new TrueOrFalse("Dinosaurs became extinct during the Cretaceous period?", 'dino/17.jpg', 'True')
 
 
 const dinoQuestions = {
     possibleAnswers: ['Triceratops', 'Tyrannosaurus-Rex', 'Brachiosaurus', 'Stegosaurus', 'Velociraptor', 'Spinosaurus', 'Ankylosaurus', 'Allosaurus'],
-    questionsArray: [dino1, dino2, dino3, dino4, dino5, dino6, dino7, dino8]
-}5
+    questionsArray: [dino1, dino2, dino3, dino4, dino5, dino6, dino7, dino8, dino9, dino10, dino11, dino12, dino13, dino14, dino15, dino16, dino17]
+}
